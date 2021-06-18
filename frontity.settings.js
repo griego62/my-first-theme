fed2cfef1ec2227b2a18fee1e7508bac86740646
griego62 <49359787+griego62@users.noreply.github.com>
2021-06-18 13:58:06 +0300

    Add files via upload

---
+++ frontity.settings.js
@@ -0,0 +1,24 @@
+const settings = {
+  "name": "hello-frontity",
+  "state": {
+    "frontity": {
+      "url": "https://test.frontity.org",
+      "title": "Test Frontity Blog",
+      "description": "WordPress installation for Frontity development"
+    }
+  },
+  "packages": [
+    {
+      "name": "my-first-theme",
+      "state": {
+        "source": {
+          "url": "https://test.frontity.org"
+        }
+      }
+    },
+    "@frontity/tiny-router",
+    "@frontity/html2react"
+  ]
+};
+
+export default settings;
